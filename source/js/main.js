34cee8ebeb63315e291a691fa7ea530d8bb133d7
Anton <kiyananton@gmail.com>
2023-02-13 17:51:55 +0300

    add stock card content

--- source/js/main.js
+++ source/js/main.js
@@ -1,1 +1,19 @@
-console.log("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM");
+const $btn = $('.breadcrumbs-stock__button');
+
+$btn.on('click', function(e) {
+  if (window.innerWidth < 1025) {
+    console.log('click');
+    $(e.currentTarget).next('.breadcrumbs-stock__download-tooltip').toggleClass('opened')
+  }
+})
+
+$(document).on('click', function(e) {
+  const $target = $(e.target);
+
+  if (
+    $target.closest('.breadcrumbs-stock__download-tooltip').length === 0 &&
+    $target.closest('.breadcrumbs-stock__button').length === 0
+  ) {
+    $('.breadcrumbs-stock__download-tooltip').removeClass('opened');
+  }
+})
